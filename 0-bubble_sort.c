0e5cadfce27d298959d892cdfb4d956443c48192
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-02-16 08:06:28 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, s2 = size;
 	int t, s;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		s = 0;
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < s2; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j - 1] > array[j])
 			{
-				t = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = t;
 				s = 1;
+				t = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = t;
 				print_array(array, size);
 			}
 		}
 		if (s == 0)
 			break;
+		s = 0;
+		s2--;
 	}
 }
